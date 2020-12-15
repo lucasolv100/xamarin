e654944d3d65e56edc571457ce5e5eea9ec7ad12
lucas <lucas@smcaetano.com.br>
2020-12-15 08:54:45 -0300

    adicionado exemplo de frame; adicionado propriedades bindable;

--- AppGallery/AppGallery/AppGallery/AppBase/Menu.xaml.cs
+++ AppGallery/AppGallery/AppGallery/AppBase/Menu.xaml.cs
@@ -93,5 +93,11 @@ namespace AppGallery.AppBase
 			((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Estilos.EstiloContent.ControlePagina();
 			((MasterDetailPage)App.Current.MainPage).IsPresented = false;
 		}
+
+        private void AbrirFrame(object sender, EventArgs e)
+        {
+			((MasterDetailPage)App.Current.MainPage).Detail = new XamarinForms.Estilos.EstiloFrame.Quadrado();
+			((MasterDetailPage)App.Current.MainPage).IsPresented = false;
+		}
     }
 }
---
+++ AppGallery/AppGallery/AppGallery/XamarinForms/Estilos/EstiloContent/CardAuthor.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGallery.XamarinForms.Estilos.EstiloContent
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardAuthor : ContentView
+    {
+
+        public static readonly BindableProperty NomeProperty = BindableProperty.Create("Nome", typeof(string), typeof(CardAuthor));
+        public static readonly BindableProperty DescricaoProperty = BindableProperty.Create("Descricao", typeof(string), typeof(CardAuthor));
+        public static readonly BindableProperty BVColorProperty = BindableProperty.Create("BVColor", typeof(Color), typeof(CardAuthor));
+
+        public string Nome { get { return (string)GetValue(NomeProperty); } set { SetValue(NomeProperty, value); } }
+        public string Descricao { get { return (string)GetValue(DescricaoProperty); } set { SetValue(DescricaoProperty, value); } }
+        public Color BVColor { get { return (Color)GetValue(BVColorProperty); } set { SetValue(BVColorProperty, value); } }
+        public CardAuthor()
+        {
+            InitializeComponent();
+        }
+    }
+}
